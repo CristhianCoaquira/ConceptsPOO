2b138ee8fbe0f9517f68fb2db07d1fb1d0adfd6a
Cristhian Coaquira <cristhiandanielcoaquira@gmail.com>
2022-03-06 17:08:43 -0400

    Implement Date class

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
 Console.WriteLine("POO Concepts!");
 Console.WriteLine("=============");
+
+try
+{
+    Date date1 = new Date(2024, 02, 29);
+    Date date2 = new Date(2022, 12, 31);
+    Date date3 = new Date(2022, 11, 30);
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+    Console.WriteLine(date3);
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
